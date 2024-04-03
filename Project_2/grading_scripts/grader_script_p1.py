eefb5db2e4dafaaedc24fb38fa7c9a8919b5c641
Neha Vadnere <nvadnere@asu.edu>
2024-04-03 12:18:03 -0700

    Grading script now allows seperate folders are objects and checks for validity of either 10 or 11 objects for each case.
    Signed-off-by: Neha Vadnere <nvadnere@asu.edu>

--- Project_2/grading_scripts/grader_script_p1.py
+++ Project_2/grading_scripts/grader_script_p1.py
@@ -77,7 +77,7 @@ class aws_grader():
             self.test_result[TC_num] = "PASS"
 
             for i,folder_n in enumerate(in_objects):
-                if len(in_objects) * 10 == len(objects):
+                if len(in_objects) * 10 == len(objects) or len(in_objects) * 11 == len(objects):
                     print(f"Number of objects matches {len(in_objects) * 10}, {len(objects)}")
                     self.test_result[TC_num] = "PASS"
                 else:
